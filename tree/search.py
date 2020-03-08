f69482c969df2dd58500f94cce164d74f97685a7
imaddevinci <imad.al_moslli@devinci.fr>
2020-03-08 13:29:32 +0100

    last version

---
+++ tree/search.py
@@ -0,0 +1,42 @@
+class MonteCarloTreeSearch(object):
+
+    def __init__(self, node):
+        """
+        MonteCarloTreeSearchNode
+        Parameters
+        ----------
+        node : mctspy.tree.nodes.MonteCarloTreeSearchNode
+        """
+        self.root = node
+
+    def best_action(self, simulations_number, c_param):
+        """
+        Parameters
+        ----------
+        simulations_number : int
+            number of simulations performed to get the best action
+        Returns
+        -------
+        """
+        for _ in range(0, simulations_number):            
+            v = self._tree_policy()
+            reward = v.rollout()
+            v.backpropagate(reward)
+        # to select best child go for exploitation only
+        return self.root.best_child(c_param=c_param)
+
+    def _tree_policy(self):
+        """
+        selects node to run rollout/playout for
+
+        Returns
+        -------
+
+        """
+        current_node = self.root
+        while not current_node.is_terminal_node():
+            if not current_node.is_fully_expanded():
+                return current_node.expand()
+            else:
+                current_node = current_node.best_child()
+        return current_node
